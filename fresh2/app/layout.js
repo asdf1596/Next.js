b66f0146ffe3bf0efafcb2393cd875f3b75cb5b0
asdf1596 <7082mc@naver.com>
2023-10-22 00:18:23 +0900

    1022-1

---
+++ fresh2/app/layout.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <head />
+            <body>
+                <div className="navbar">
+                    <Link href="/">Home</Link>
+                    <Link href="/list">List</Link>
+                </div>
+                {children}
+            </body>
+        </html>
+    );
+}
--- fresh2/app/page.js
+++ fresh2/app/page.js
@@ -3,10 +3,6 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main>
-            <div className="navbar">
-                <Link href="/">home</Link>
-                <Link href="/list">list페이지</Link>
-            </div>
             <h1 className="title">Programming Log</h1>
             <p className="title-sub">by </p>
         </main>
